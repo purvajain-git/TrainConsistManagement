e9653c82b5d492d1cda69aff6a94ecd377173be0
PURVA JAIN <pj8602@srmist.edu.in>
2026-03-20 15:37:43 +0530

    usecase4

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,18 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TrainConsistManagement {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        Set<String> bogieIDs = new HashSet<>();
+        List<String> trainConsist = new LinkedList<>();
 
-        bogieIDs.add("BG101");
-        bogieIDs.add("BG102");
-        bogieIDs.add("BG101");
-        bogieIDs.add("BG103");
-        bogieIDs.add("BG102");
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
 
-        System.out.println("Unique bogie IDs: " + bogieIDs);
+        trainConsist.add(2, "Pantry Car");
+
+        trainConsist.remove(0);
+        trainConsist.remove(trainConsist.size() - 1);
+
+        System.out.println("Final ordered train consist: " + trainConsist);
     }
 }
